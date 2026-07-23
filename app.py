1792e1cfa586545331b46488f5cfc6ae5cb363f9
e.baruti <63745689+xoniks@users.noreply.github.com>
2026-07-23 20:51:42 +0200

    random forest added

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 st.title('Simple ML app for house price')
 
-model = joblib.load("iowa_model.pkl")
+model_dt = joblib.load("models/iowa_model.pkl")
+model_rf = joblib.load("models/iowa_model_rf.pkl")
 features = joblib.load("iowa_features.pkl")
 
 # ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
@@ -18,10 +19,18 @@ full_bath = st.number_input('Full Bath', value=2)
 bedroom_above_gr = st.number_input('Bedroom Above Ground', value=3)
 total_rooms_above_grd = st.number_input('Total Rooms Above Ground', value=6)
 
-if st.button("Predict price"):
+if st.button("Predict price with decession tree"):
     #transform this to df for predictions
     input_data_list = [lot_area, year_built, first_floor_sf, second_floor_sf, full_bath,
                     bedroom_above_gr, total_rooms_above_grd]
     input_df = pd.DataFrame([input_data_list] ,columns=features)
-    prediction = model.predict(input_df)[0]
+    prediction = model_dt.predict(input_df)[0]
     st.write(f"The predicted price is {prediction}")
+    
+if st.button("Predict price with decession random forest"):
+    #transform this to df for predictions
+    input_data_list = [lot_area, year_built, first_floor_sf, second_floor_sf, full_bath,
+                    bedroom_above_gr, total_rooms_above_grd]
+    input_df = pd.DataFrame([input_data_list] ,columns=features)
+    prediction = model_rf.predict(input_df)[0]
+    st.write(f"The predicted price is $ **{prediction}** ")
